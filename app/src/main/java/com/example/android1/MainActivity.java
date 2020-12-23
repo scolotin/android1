9fdc38550e846e9ba5d5f2d54325c1a1eb4497cf
Scolotin Alexey <scolotin@gmail.com>
2020-12-24 00:15:44 +0300

    Добавлен макет экрана настроек

--- app/src/main/java/com/example/android1/MainActivity.java
+++ app/src/main/java/com/example/android1/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.android1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.add_city);
+        setContentView(R.layout.activity_main);
+
+        Button btnAddCity = findViewById(R.id.btn_add_city);
+        btnAddCity.setOnClickListener(v -> setContentView(R.layout.add_city));
     }
 }
