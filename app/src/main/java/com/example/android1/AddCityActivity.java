7b33a12f40c162a4770befce0d05ebd004e38f5f
Scolotin Alexey <scolotin@gmail.com>
2021-01-16 11:22:04 +0300

    Добавлено сохранение состояния строки поиска города

--- app/src/main/java/com/example/android1/AddCityActivity.java
+++ app/src/main/java/com/example/android1/AddCityActivity.java
@@ -13,6 +13,8 @@ import android.widget.Button;
 import android.widget.SearchView;
 
 public class AddCityActivity extends AppCompatActivity {
+    private SearchView searchView;
+    private String cityNameRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +23,7 @@ public class AddCityActivity extends AppCompatActivity {
 
         Button btnApply = findViewById(R.id.btn_apply);
         btnApply.setOnClickListener(v -> {
-            SearchView searchView = findViewById(R.id.search);
+            searchView = findViewById(R.id.search);
             Intent cityNameIntent = new Intent();
             cityNameIntent.putExtra("cityName", searchView.getQuery().toString());
             setResult(RESULT_OK, cityNameIntent);
@@ -33,6 +35,14 @@ public class AddCityActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.add_city_menu, menu);
+
+        if (cityNameRequest != null && !cityNameRequest.isEmpty()) {
+            MenuItem searchMenuItem = menu.findItem(R.id.search);
+            searchView = searchMenuItem.getActionView().findViewById(R.id.search);
+            searchView.setIconified(false);
+            searchView.setQuery(cityNameRequest, true);
+            searchView.clearFocus();
+        }
 
         return super.onCreateOptionsMenu(menu);
     }
@@ -48,5 +58,21 @@ public class AddCityActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        searchView = findViewById(R.id.search);
+        outState.putString("cityNameRequest", searchView.getQuery().toString());
+    }
+
+    @SuppressLint("DefaultLocale")
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        cityNameRequest = savedInstanceState.getString("cityNameRequest");
     }
 }
